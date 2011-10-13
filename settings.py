9a1075dfd88b83171369d9b442c5ed5fbed193ad
Christos Psaltis <cpsaltis@unweb.me>
2011-10-13 15:47:55 +0300

    change naming

--- settings.py
+++ settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'blog.urls'
+ROOT_URLCONF = 'simple-archive.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
